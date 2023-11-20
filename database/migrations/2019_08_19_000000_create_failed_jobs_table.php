b1157548cafcdc58b52435e939ffa93b1a0fddf0
Boiko Ihor <ihor.boiko@appflame.com>
2023-11-20 09:55:33 +0200

    failed jobs migration

--- database/migrations/2019_08_19_000000_create_failed_jobs_table.php
+++ database/migrations/2019_08_19_000000_create_failed_jobs_table.php
@@ -1,32 +1,37 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+    private const TABLE = 'failed_jobs';
+
     /**
-     * Run the migrations.
+     * @return void
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
-            $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
-        });
+        Schema::create(
+            self::TABLE,
+            function (Blueprint $table) {
+                $table->id();
+                $table->longText('exception');
+                $table->unsignedFloat('failed_at');
+                $table->text('connection');
+                $table->longText('payload');
+                $table->text('queue');
+                $table->string('uuid')->unique();
+            }
+        );
     }
 
     /**
-     * Reverse the migrations.
+     * @return void
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists(self::TABLE);
     }
 };
