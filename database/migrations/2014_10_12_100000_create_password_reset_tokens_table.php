9428b155f1845349426de65cb4fb6f5ff95c97cf
Boiko Ihor <ihor.boiko@appflame.com>
2023-11-20 10:15:31 +0200

    password reset token migration

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -1,28 +1,30 @@
 <?php
+declare(strict_types=1);
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
+    private const TABLE = 'password_reset_tokens';
+
     /**
-     * Run the migrations.
+     * @return void
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
+        Schema::create(self::TABLE, function (Blueprint $table) {
+            $table->unsignedFloat('created_at', 17, 6);
             $table->string('email')->primary();
             $table->string('token');
-            $table->timestamp('created_at')->nullable();
         });
     }
 
     /**
-     * Reverse the migrations.
+     * @return void
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists(self::TABLE);
     }
 };
