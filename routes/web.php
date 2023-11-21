2ee742ad4cca59641392a3285f23ee4ca2c0f888
Boiko Ihor <ihor.boiko@appflame.com>
2023-11-21 15:56:00 +0200

    react + ts

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,8 +15,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/queue', [\App\Http\Controllers\QueueController::class, 'index']);
+Route::get('{react}', function () {
+    return view('index');
+})->where('react', '^((?!api).)*$');
