8fde8b5c3c597d352d3be8fc4a50e241f95e60c7
Ihor Boiko <ihor.boiko@appflame.com>
2023-11-30 16:40:34 +0200

    country admin crud

---
+++ database/migrations/2023_11_30_130534_create_countries_table.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+use App\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    private const TABLE = 'countries';
+
+    /**
+     * @return void
+     */
+    public function up(): void
+    {
+        if (false === Schema::hasTable(self::TABLE)) {
+            Schema::create(
+                self::TABLE,
+                function (Blueprint $table) {
+                    $table->id();
+                    $this->floatDate('created_at', $table);
+                    $table->string('name')->unique();
+                    $this->floatDate('updated_at', $table);
+                }
+            );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
