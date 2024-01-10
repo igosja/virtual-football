cda835c64040e001c5a9422a1d84bb9604fa5796
Ihor Boiko <ihor.boiko@appflame.com>
2024-01-10 16:56:38 +0200

    game

---
+++ database/migrations/2024_01_10_144634_create_games_table.php
@@ -0,0 +1,40 @@
+<?php
+declare(strict_types=1);
+
+use App\Database\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    private const TABLE = 'games';
+
+    /**
+     * @return void
+     */
+    public function up(): void
+    {
+        if (false === Schema::hasTable(self::TABLE)) {
+            Schema::create(
+                self::TABLE,
+                function (Blueprint $table) {
+                    $table->id();
+                    $this->floatDate('created_at', $table);
+                    $table->foreignId('guest_team_id')->references('id')->on('teams');
+                    $table->foreignId('home_team_id')->references('id')->on('teams');
+                    $table->boolean('is_played')->default(false);
+                    $table->foreignId('schedule_id')->constrained();
+                    $table->foreignId('stadium_id')->unique()->references('id')->on('stadiums');
+                    $this->floatDate('updated_at', $table);
+                }
+            );
+        }
+    }
+
+    /**
+     * @return void
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists(self::TABLE);
+    }
+};
