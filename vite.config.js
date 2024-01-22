18f73c4ed04c3563383d2ce443f312fc4a13412f
Ihor Boiko <ihor.boiko@appflame.com>
2024-01-22 09:43:49 +0200

    vite config security alert

--- vite.config.js
+++ vite.config.js
@@ -7,4 +7,9 @@ export default defineConfig({
         laravel(['resources/js/index.tsx']),
         react(),
     ],
+    server: {
+        fs: {
+            deny: ['.env', '.env.*']
+        }
+    }
 });
